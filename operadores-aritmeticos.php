01185da45299a0af7d38acd191f5851ed152d978
Libardo Arroyave <laastos@gmail.com>
2022-11-09 18:47:23 -0500

    Operadores aritmeticos

---
+++ operadores-aritmeticos.php
@@ -0,0 +1,23 @@
+<?php
+$valorA = 5;
+$valorB = 2;
+
+echo "Operaciones entre los numeros $valorA y $valorB\n\n";
+
+echo "Suma => ";
+var_dump($valorA + $valorB);
+
+echo "Resta => ";
+var_dump($valorA - $valorB);
+
+echo "Multiplicacion => ";
+var_dump($valorA * $valorB);
+
+echo "Division => ";
+var_dump($valorA / $valorB);
+
+echo "Potenciacion => ";
+var_dump($valorA ** $valorB);
+
+echo "Modulo => ";
+var_dump($valorA % $valorB);
