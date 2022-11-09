e4eb67996a54f7f3bdd69ccd1dad8b097a43b2ba
Libardo Arroyave <laastos@gmail.com>
2022-11-09 18:43:13 -0500

    Operadores lógicos

---
+++ operadores-logicos.php
@@ -0,0 +1,14 @@
+<?php
+$valorA = true;
+$valorB = false;
+
+echo "AND => ";
+var_dump($valorA and $valorB);
+echo "\n";
+
+echo "OR => ";
+var_dump($valorA or $valorB);
+echo "\n";
+
+echo "XOR => ";
+var_dump($valorA xor $valorB);
